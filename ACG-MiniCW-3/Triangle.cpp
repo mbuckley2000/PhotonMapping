993c07939c01ef07d391d82fc2df78930b142247
Matt Buckley <mbuckley2000@gmail.com>
2018-10-29 18:00:30 +0000

    Renamed some variables for clarity in triangle ray intersection.
    Set up objects for hand-in
    
    
    Former-commit-id: 4ef0d79bafd25f7c586ae2109d315d8b0229778e
    Former-commit-id: baed217db7832ecb8ebea608b074455b71a4c267

--- ACG-MiniCW-3/Triangle.cpp
+++ ACG-MiniCW-3/Triangle.cpp
@@ -3,49 +3,56 @@
 
 using namespace Eigen;
 
+const double EPSILON = 0.0000001;
+const bool CULLING = true;
 
-
-const double kEpsilon = 0.0000001;
-const bool culling = true;
-
-//From https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
+//https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
 bool Triangle::rayIntersects(Ray & ray, float& t, float& u, float& v)
 {
-	const Vector3f orig = ray.position;
-	const Vector3f dir = ray.direction;
+	//Triangle ray intersection
+	const Vector3f rayPos = ray.position;
+	const Vector3f rayDir = ray.direction;
 	const Vector3f v0 = this->vertices.col(0);
-	const Vector3f v1 = this->vertices.col(1);
-	const Vector3f v2 = this->vertices.col(2);
-
 	const Vector3f edge1 = this->edge1;
 	const Vector3f edge2 = this->edge2;
 
-	const Vector3f pvec = dir.cross(edge2);
-	const float det = edge1.dot(pvec);
+	const Vector3f pVec = rayDir.cross(edge2);
 
-	if (culling) {
-		// if the determinant is negative the triangle is backfacing
-		// if the determinant is close to 0, the ray misses the triangle
-		if (det < kEpsilon) return false;
+	const float det = edge1.dot(pVec);
+
+	if (CULLING) {
+		// Negative det means the triangle isn't facing the camera
+		// Det near 0 is a miss
+		if (det < EPSILON) {
+			return false;
+		}
 	}
 	else {
-		// ray and triangle are parallel if det is close to 0
-		if (fabs(det) < kEpsilon) return false;
+		// They are parallel
+		if (fabs(det) < EPSILON) {
+			return false;
+		}
 	}
 
-	float invDet = 1 / det;
+	float detInv = 1 / det;
 
-	const Vector3f tvec = orig - v0;
+	const Vector3f tVec = rayPos - v0;
 
-	u = tvec.dot(pvec) * invDet;
-	if (u < 0 || u > 1) return false;
+	u = tVec.dot(pVec) * detInv;
 
-	const Vector3f qvec = tvec.cross(edge1);
+	if (u < 0 || u > 1) {
+		return false;
+	}
 
-	v = dir.dot(qvec) * invDet;
-	if (v < 0 || u + v > 1) return false;
+	const Vector3f qvec = tVec.cross(edge1);
 
-	t = edge2.dot(qvec) * invDet;
+	v = rayDir.dot(qvec) * detInv;
+
+	if (v < 0 || u + v > 1) {
+		return false;
+	}
+
+	t = edge2.dot(qvec) * detInv;
 
 	return true;
 }
@@ -53,91 +60,12 @@ bool Triangle::rayIntersects(Ray & ray, float& t, float& u, float& v)
 Triangle::Triangle(Eigen::Matrix3f vertices)
 {
 	this->vertices = vertices;
+
+	//Pre-calculate edges for efficiency
 	this->edge1 = vertices.col(1) - vertices.col(0);
 	this->edge2 = vertices.col(2) - vertices.col(0);
-
 }
-
 
 Triangle::~Triangle()
 {
 }
-
-
-/*
-#include "pch.h"
-#include "Triangle.h"
-#include <glm/vec3.hpp>
-#include <glm/geometric.hpp>
-
-#define CULLING true
-#define EPSILON 0.0000001
-
-//From https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
-bool Triangle::rayIntersects(Ray & ray, float& t, float& u, float& v)
-{
-	const Vector3f orig = ray.position;
-	const Vector3f dir = ray.direction;
-	const Vector3f v0 = this->vertex[0];
-
-	const Vector3f edge1 = this->edge1;
-	const Vector3f edge2 = this->edge2;
-
-	const Vector3f pvec = glm::cross(dir, edge2);
-	const float det = glm::dot(edge1, pvec);
-
-	if (CULLING) {
-		// if the determinant is negative the triangle is backfacing
-		// if the determinant is close to 0, the ray misses the triangle
-		if (det < EPSILON) return false;
-	}
-	else {
-		// ray and triangle are parallel if det is close to 0
-		if (fabs(det) < EPSILON) return false;
-	}
-
-	float invDet = 1 / det;
-
-	const Vector3f tvec = orig - v0;
-
-	u =  glm::dot(tvec, pvec) * invDet;
-	if (u < 0 || u > 1) return false;
-
-	const Vector3f qvec = glm::cross(tvec, edge1);
-
-	v = glm::dot(dir, qvec) * invDet;
-	if (v < 0 || u + v > 1) return false;
-
-	t = glm::dot(edge2, qvec) * invDet;
-
-	return true;
-}
-
-
-
-Triangle::Triangle(Vector3f v0, Vector3f v1, Vector3f v2)
-{
-	this->vertex[0] = v0;
-	this->vertex[1] = v1;
-	this->vertex[2] = v2;
-	this->calculatePlane();
-}
-
-
-Triangle::~Triangle()
-{
-}
-
-void Triangle::calculatePlane()
-{
-	//Calculate edges
-	this->edge1 = this->vertex[1] - this->vertex[0];
-	this->edge2 = this->vertex[2] - this->vertex[0];
-
-	//Calculate normal
-	this->normal = glm::cross(this->edge1, this->edge2);
-
-	//Point is one of the vertices
-	this->point = vertex[0];
-}
-*/
